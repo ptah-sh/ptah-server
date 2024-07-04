a501e8d4d90ea75776a9f9123f2c742b6d69274d
Bohdan Shulha <b.shulha@pm.me>
2024-07-04 14:59:17 +0200

    feat: #30 try add trusted proxies 3

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,6 +18,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies('*');
+
         $middleware
             ->web(append: [
                 HandleInertiaRequests::class,
