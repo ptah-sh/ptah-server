bae520b518e0e1d36a8fc9cc24a8821b53caa531
Bohdan Shulha <b.shulha@pm.me>
2024-08-28 18:19:47 +0200

    fix: pricing on the billing page

--- config/billing.php
+++ config/billing.php
@@ -5,7 +5,7 @@ return [
         'plans' => [
             [
                 'name' => 'Hobby',
-                'price' => 19,
+                'price' => 9,
                 'description' => 'Perfect plan to try the service or host non-critical applications',
                 'product_id' => env('PADDLE_PLAN_HOBBY_PRODUCT_ID'),
                 'price_id' => env('PADDLE_PLAN_HOBBY_PRICE_ID'),
@@ -18,7 +18,7 @@ return [
             ],
             [
                 'name' => 'Startup',
-                'price' => 49,
+                'price' => 19,
                 'description' => 'Need more power or an improved stability? This is your choice!',
                 'product_id' => env('PADDLE_PLAN_STARTUP_PRODUCT_ID'),
                 'price_id' => env('PADDLE_PLAN_STARTUP_PRICE_ID'),
