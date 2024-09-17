82c068726789573d12640fde3f028e5e43071a97
Bohdan Shulha <b.shulha@pm.me>
2024-09-17 23:11:57 +0200

    feat: pricing test

--- config/billing.php
+++ config/billing.php
@@ -7,7 +7,7 @@ return [
         'plans' => [
             [
                 'name' => 'Hobby',
-                'price' => 9,
+                'price' => 14,
                 'description' => 'Perfect plan to try the service or host non-critical applications',
                 'product_id' => env('PADDLE_PLAN_HOBBY_PRODUCT_ID'),
                 'price_id' => env('PADDLE_PLAN_HOBBY_PRICE_ID'),
@@ -20,7 +20,7 @@ return [
             ],
             [
                 'name' => 'Startup',
-                'price' => 19,
+                'price' => 29,
                 'description' => 'Need more power or an improved stability? This is your choice!',
                 'product_id' => env('PADDLE_PLAN_STARTUP_PRODUCT_ID'),
                 'price_id' => env('PADDLE_PLAN_STARTUP_PRICE_ID'),
